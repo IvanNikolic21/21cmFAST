1a063f2b3eb13ae448e69dbce3252c7d896c1019
IvanNikolic21 <ivan.nikolic@sns.it>
2021-07-21 16:06:07 +0200

    fixed test

--- tests/test_ksz.py
+++ tests/test_ksz.py
@@ -5,7 +5,20 @@ import numpy as np
 import py21cmfast as p21c
 
 
-def test_ksz(lc):
-    ksz = p21c.run_kSZ(lc)
+@pytest.fixture(scope="session")
+def lc_ksz(perturb_field, max_redshift):
+    return p21c.run_lightcone(
+        perturb=perturb_field,
+        max_redshift=max_redshift,
+        lightcone_quantites=(
+            "density",
+            "velocity",
+            "xH_box",
+        ),
+    )
+
+
+def test_ksz(lc_ksz):
+    ksz = p21c.run_kSZ(lc_ksz)
     assert not any(np.isnan(np.array(ksz.l_s))) and len(ksz.l_s) > 0
-    assert np.shape(ksz.kSZ_box) == (lc.HII_DIM, lc.HII_DIM)
+    assert np.shape(ksz.kSZ_box) == (lc_ksz.HII_DIM, lc_ksz.HII_DIM)
