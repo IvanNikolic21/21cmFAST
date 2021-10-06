682588826ae91d235d95adb6e215527091d794ad
Ivan Nikolic <inikolic@trantor01.hpc.sns.it>
2021-10-06 17:22:44 +0200

    corrected the warning for OUTPUT_ALL_VEL

--- src/py21cmfast/wrapper.py
+++ src/py21cmfast/wrapper.py
@@ -3343,7 +3343,7 @@ def run_kSZ(
         cosmo_params = lc.cosmo_params
         astro_params = lc.astro_params
         flag_options = lc.flag_options
-        if hasattr(lc, "velocity_x") is False or (lc, "velocity_y") is False:
+        if user_params.OUTPUT_ALL_VEL is False:
             logger.warning(
                 "Using all velocity components is advised with OUTPUT_ALL_VEL=True."
             )
